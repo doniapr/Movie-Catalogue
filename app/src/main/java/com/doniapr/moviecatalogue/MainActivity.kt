3eab886573923930a735c3d73b2745b6240e54ca
Doni Apr <doniapr14@gmaiil.com>
2020-12-04 15:35:44 +0700

    Add: Fragment movie, tv show and favorite

--- app/src/main/java/com/doniapr/moviecatalogue/MainActivity.kt
+++ app/src/main/java/com/doniapr/moviecatalogue/MainActivity.kt
@@ -1,48 +1,52 @@
 package com.doniapr.moviecatalogue
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.doniapr.core.data.Resource
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
-import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
-    private val mainViewModel: MainViewModel by viewModel()
+    private val navigationListener: BottomNavigationView.OnNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            val fragment: Fragment
+            when (item.itemId) {
+                R.id.menu_movie -> {
+                    fragment = MovieFragment()
+                    changeFragment(fragment)
 
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.menu_tv_show -> {
+                    fragment = TvShowFragment()
+                    changeFragment(fragment)
+
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.menu_favorite -> {
+                    fragment = FavoriteFragment()
+                    changeFragment(fragment)
+
+                    return@OnNavigationItemSelectedListener true
+                }
+                else -> throw Throwable("Unknown item Id")
+            }
+        }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val movieAdapter = MovieAdapter()
-
-        mainViewModel.movies.observe(this, Observer { movie ->
-            if (movie != null) {
-                when (movie) {
-                    is Resource.Loading -> progress_bar.visibility = View.VISIBLE
-                    is Resource.Success -> {
-                        progress_bar.visibility = View.GONE
-                        rv_movie.visibility = View.VISIBLE
-                        txt_error.visibility = View.GONE
-                        movieAdapter.setData(movie.data)
-                    }
-                    is Resource.Error -> {
-                        progress_bar.visibility = View.GONE
-                        rv_movie.visibility = View.GONE
-                        txt_error.visibility = View.VISIBLE
-                        txt_error.text = movie.message ?: "Oops.. something went wrong"
-                    }
-                }
-            }
-        })
-
-        with(rv_movie) {
-            layoutManager = LinearLayoutManager(context)
-            setHasFixedSize(true)
-            adapter = movieAdapter
+        nav_view.setOnNavigationItemSelectedListener(navigationListener)
+        if (savedInstanceState == null) {
+            nav_view.selectedItemId = R.id.menu_movie
         }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frame_container, fragment, fragment.javaClass.simpleName)
+            .commit()
     }
 }
